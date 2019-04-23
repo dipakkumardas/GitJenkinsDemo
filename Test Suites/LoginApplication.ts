40f3b4bc32040715ba2698e8581d87977b4ab147
unknown <dipak@DIPAKDAS-PC.indusnettechnologies.com>
2019-04-23 16:43:11 +0530

    First Upload

---
+++ Test Suites/LoginApplication.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>This is Login Application Suite.</description>
+   <name>LoginApplication</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>af2bca27-e49e-4f34-ab26-46dc0bb4eda8</testSuiteGuid>
+   <testCaseLink>
+      <guid>5a90fed4-abe4-46ff-b0e0-e9b7482d5e2f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginApplication</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
